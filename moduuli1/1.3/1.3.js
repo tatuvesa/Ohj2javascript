07b3b83f83b19057b03dc4fde686bfed95dff44c
tatuvesa <tatuve@metropolia.fi>
2024-04-05 10:13:10 +0200

    1.1 ja 1.2

---
+++ moduuli1/1.3/1.3.js
@@ -0,0 +1,7 @@
+'use strict';
+const num1 = parseFloat(prompt('First number:  '));
+const num2 = parseFloat(prompt('Second number: '));
+const num3 = parseFloat(prompt('Third number: '));
+
+const sum = num1 + num2 + num3;
+console.log('Summa on ' + sum);
