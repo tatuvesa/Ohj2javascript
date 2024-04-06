6bc351e2a82fc414806f61337edbbcc34446523c
tatuvesa <tatuve@metropolia.fi>
2024-04-06 16:09:37 +0200

    syy

--- moduuli1/1.5/1.5.js
+++ moduuli1/1.5/1.5.js
@@ -2,15 +2,15 @@
 const year = parseInt(prompt('Submit a year: '));
 let isLeap = false;
 
-  if (year % 4 === 0) {
-    if (year % 100 === 0) {
-      if (year % 400 === 0) {
-        isLeap = true;
-      }
-    } else {
+if (year % 4 === 0) {
+  if (year % 100 === 0) {
+    if (year % 400 === 0) {
       isLeap = true;
     }
+  } else {
+    isLeap = true;
   }
+}
 
 let result;
 if (isLeap) {
