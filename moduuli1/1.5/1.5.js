8b4113e0300dd1e53093ccc291dbd86a94b0d5a7
tatuvesa <tatuve@metropolia.fi>
2024-04-06 16:09:00 +0200

    test

--- moduuli1/1.5/1.5.js
+++ moduuli1/1.5/1.5.js
@@ -1,13 +1,4 @@
-/*
-Write a program that asks the user to enter a year 
-and notifies the user whether the input year is a leap year. 
-A year is a leap year if it is divisible by four. 
-However, years divisible by 100 are leap years only if they are also divisible by 400. 
-Print the result on the HTML document. (3p)
-*/
-
 'use strict';
-
 const year = parseInt(prompt('Submit a year: '));
 let isLeap = false;
 
@@ -21,5 +12,11 @@ let isLeap = false;
     }
   }
 
-const result = isLeap ? `${year} is a leap year.` : `${year} is not a leap year.`;
+let result;
+if (isLeap) {
+  result = year + " is a leap year.";
+} else {
+  result = year + " is not a leap year.";
+}
+
 document.getElementById("result").innerHTML = result;
