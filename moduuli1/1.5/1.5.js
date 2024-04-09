bd2fa70e3ed97fd52a95da61d9ce507c469e1213
tatuvesa <tatuve@metropolia.fi>
2024-04-09 09:36:38 +0300

    uus

--- moduuli1/1.5/1.5.js
+++ moduuli1/1.5/1.5.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 const year = parseInt(prompt("Submit a year: "));
 const isLeap = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 document.getElementById("result").innerHTML = `${year} is ${isLeap ? "" : "not "}a leap year.`;
