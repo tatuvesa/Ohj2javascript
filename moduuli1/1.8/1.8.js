34e97a9c294fe8ef9b469544ffaff6bcf9343f6a
tatuvesa <tatuve@metropolia.fi>
2024-04-10 15:48:21 +0300

    uus

--- moduuli1/1.8/1.8.js
+++ moduuli1/1.8/1.8.js
@@ -3,9 +3,9 @@ const endYear = prompt("Please enter the end year", "2020");
 let leaps = "";
 
 for (let year = startYear; year <= endYear; year++) {
-  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    leaps += `<li>${year}</li>`;
-  }
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        leaps += `<li>${year}</li>`;
+    }
 }
 
 document.getElementById("result").innerHTML = leaps;
