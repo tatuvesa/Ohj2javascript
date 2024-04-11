5174010a9a5668648df5195bef594a25cd32f964
tatuvesa <tatuve@metropolia.fi>
2024-04-11 12:14:26 +0300

    dgrdrg

--- moduuli2/2.6/2.6.js
+++ moduuli2/2.6/2.6.js
@@ -1,31 +1,4 @@
-/* Write a function that returns a random dice roll between 1 and 6.
-The function should not have any parameters.
-Write a main program that rolls the dice until the result is 6.
-The main program should print out the result of each roll in an unordered list (<ul>). (2p)
-*/
-
 'use strict'
-/* function dice() {
-    return Math.floor(Math.random() * 6) + 1;
-}
-
-let roll = dice();
-let ul = document.createElement('ul');
-
-while (roll !== 6) {
-    let li = document.createElement('li');
-    li.textContent = roll;
-    ul.appendChild(li);
-    roll = dice();
-}
-
-let li = document.createElement('li');
-li.textContent = roll;
-ul.appendChild(li);
-
-document.body.appendChild(ul);
-*/
-
 function diceRoll() {
     return Math.floor(Math.random() * 6) + 1;
 }
