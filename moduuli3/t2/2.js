bd9ef4eab7d24d4e377373c8b6b197c9c08c8340
tatuvesa <tatuve@metropolia.fi>
2024-04-17 17:39:15 +0300

    uus

---
+++ moduuli3/t2/2.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    const targetElement = document.getElementById("target");
+    const ul = document.createElement("ul");
+
+    const firstItem = document.createElement("li");
+    firstItem.textContent = "First item";
+    ul.appendChild(firstItem);
+
+    const secondItem = document.createElement("li");
+    secondItem.textContent = "Second item";
+    secondItem.classList.add("my-item");
+    ul.appendChild(secondItem);
+
+    const thirdItem = document.createElement("li");
+    thirdItem.textContent = "Third item";
+    ul.appendChild(thirdItem);
+
+    targetElement.appendChild(ul);
+};
