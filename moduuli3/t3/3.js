387ebf7e49eb0f04841d7eff14c39cfc9d26ecc8
tatuvesa <tatuve@metropolia.fi>
2024-04-18 12:37:58 +0300

    uus

--- moduuli3/t3/3.js
+++ moduuli3/t3/3.js
@@ -1,2 +1,19 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+const targetElement = document.getElementById('target');
+const ul = document.createElement('ul');
+
+/* for (let i = 0; i < names.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = names[i];
+    ul.appendChild(li);
+}
+*/
+
+names.forEach(name => {
+    const li = document.createElement('li');
+    li.textContent = name;
+    ul.appendChild(li);
+});
+
+targetElement.appendChild(ul);
