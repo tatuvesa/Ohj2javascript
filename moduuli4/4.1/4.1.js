bb3f0e5cbfbb55bcadba98535a7fa0813cfc88c0
tatuvesa <tatuve@metropolia.fi>
2024-04-25 14:40:28 +0300

    Update searchForm event listener to prevent form submission in 4.1.js

--- moduuli4/4.1/4.1.js
+++ moduuli4/4.1/4.1.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const resultDiv = document.getElementById('result');
 
     searchForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the form from submitting normally
+        event.preventDefault();
         const query = queryInput.value;
         fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data); // Log the data to the console
-                resultDiv.textContent = JSON.stringify(data, null, 2); // Display the data in the page
+                console.log(data);
+                resultDiv.textContent = JSON.stringify(data, null, 2);
             })
             .catch(error => console.error('Error:', error));
     });
