47a2f08a4bedcfddbaee8a66ec58c2c3ed7639b1
tatuvesa <tatuve@metropolia.fi>
2024-04-25 14:52:26 +0300

    Update searchForm event listener to use fetch API in 4.1.js

--- moduuli4/4.1/4.1.js
+++ moduuli4/4.1/4.1.js
@@ -1,17 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const searchForm = document.getElementById('searchForm');
-    const queryInput = document.getElementById('query');
-    const resultDiv = document.getElementById('result');
+const searchForm = document.getElementById('searchForm');
+const queryInput = document.getElementById('query');
+const resultDiv = document.getElementById('result');
 
-    searchForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const query = queryInput.value;
-        fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                resultDiv.textContent = JSON.stringify(data, null, 2);
-            })
-            .catch(error => console.error('Error:', error));
-    });
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const query = queryInput.value;
+    fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            resultDiv.textContent = JSON.stringify(data, null, 2);
+        })
+        .catch(error => console.error('Error:', error));
 });
