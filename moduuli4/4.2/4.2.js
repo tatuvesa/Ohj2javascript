9823215afc1157264e2b711c44f67568fc614254
tatuvesa <tatuve@metropolia.fi>
2024-04-25 15:12:04 +0300

    Update searchForm event listener to use fetch API

--- moduuli4/4.2/4.2.js
+++ moduuli4/4.2/4.2.js
@@ -4,12 +4,11 @@ const resultDiv = document.getElementById('result');
 
 searchForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    const query = queryInput.value;
-    fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
+    const value_from_input = queryInput.value;
+    fetch(`https://api.tvmaze.com/search/shows?q=${value_from_input}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            resultDiv.textContent = JSON.stringify(data, null, 2);
         })
         .catch(error => console.error('Error:', error));
 });
